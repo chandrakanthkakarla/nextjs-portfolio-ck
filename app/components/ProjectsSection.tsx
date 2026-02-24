1ba567ed072a58627e07966d5f72780e2d49dd23
Chandrakanth <chandrakanth.kakarla96@gmail.com>
2026-02-24 22:18:05 +0530

    new changesss

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -14,75 +14,16 @@ type Project = {
   date: string;
 };
 const projects: Project[] = [
-  {
-    name: "Amazon SP Manager",
-    description:
-      "An app that uses Amazon's SP-API to automate and optimize inventory management for sellers.",
-    image: "/img/sp-manager.webp",
-    github: false,
-    view: false,
-    status: true,
-    date: "May - 2025",
-  },
-  {
-    name: "Hinez",
-    description:
-      "Unlock comprehensive efficiency and growth potential. Transform your operations today to boost workforce engagement and productivity.",
-    image: "/img/hinez.jpg",
-    github: false,
-    view: "https://myhinez.com/",
-    status: false,
-    date: "May - 2024",
-  },
-  {
-    name: "Rugr",
-    description:
-      "Rugr plays a crucial role in the modern financial ecosystem by facilitating interactions between merchants and related stakeholders.",
-    image: "/img/rugr.webp",
-    github: false,
-    view: "https://rugr.com",
-    status: false,
-    date: "February - 2025",
-  },
-  {
-    name: "Portfolio",
-    description:
-      "A personal portfolio website designed to display my development skills and projects.",
-    image: "/img/portfolio.webp",
-    github: "https://github.com/Akshayp2002/personal-portfolio-angular",
-    view: "https://devakshay.vercel.app",
-    status: false,
-    date: "August - 2024",
-  },
-  {
-    name: "Netflix Clone",
-    description:
-      "A Netflix clone that allows browsing and viewing TV shows using data from an open API, featuring a responsive and user-friendly interface.",
-    image: "/img/netflix-cole.webp",
-    github: "https://github.com/Akshayp2002/netflix-clone-angular",
-    view: "https://netflix-clone-angular2.vercel.app",
-    status: false,
-    date: "June - 2024",
-  },
-  {
-    name: "Swiftrevel",
-    description: "Swiftrevel champions frictionless fintech, integrating flawlessly with your existing tools and software to eliminate data silos and streamline your workflow.",
-    image: "/img/swiftrevel.webp",
-    github: false,
-    view: "https://swiftrevel.com",
-    status: false,
-    date: "May - 2024",
-  },
-  {
-    name: "Student 360",
-    description: "A final year project: A student management portal designed to efficiently manage attendance, grades, student details, and other academic information.",
-    image: "/img/student360.webp",
-    github: "https://github.com/Akshayp2002/Student-360",
-    view: false,
-    status: false,
-    date: "April - 2022",
-  },
-
+{
+  name: "NOC Operations",
+  description:
+    "Network Operations Center (NOC) Operations involves monitoring, managing, and maintaining an organization’s network and infrastructure to ensure maximum uptime, performance, and reliability.",
+  image: "/img/noc.jpg",
+  github: false,
+  view: false,
+  status: true,
+  date: "2026-02-24"
+}
 ];
 
 
